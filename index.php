600d13c42ceff523c215d54ee37c92f7633f7a87
Vanvala <geos79@tut.by>
2022-06-22 12:39:52 +0300

    initial php setup

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Store</title>
+</head>
+<body>
+    <?php 
+    // Using 2 forward slashes we create comments in php.
+    // Within the php tag, we can insert values into our HTML.
+    echo "<h1>Welcome!</h1>";
+    ?>
+</body>
+</html>
